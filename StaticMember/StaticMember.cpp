96c0cdf38a230615a341346e11a16325a31ceb2b
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-05-17 15:43:06 +0700

    Static Member

--- StaticMember/StaticMember.cpp
+++ StaticMember/StaticMember.cpp
@@ -10,6 +10,7 @@ public:
 
 	void setID();
 	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
 
 int mahasiswa::nim = 0;
@@ -23,4 +24,19 @@ void mahasiswa::printAll() {
 	cout << "Nama = " << nama << endl;
 	cout << "NIM = " << nim << endl;
 	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mhs2.nim = 10;
+	mahasiswa mhs3("Andi Kurniawan");
+	mhs3.nim = 20;
+	mahasiswa mhs4("Joko Purbo");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
 }

1d598f69571e28403bb6c007a54351cc826ae93a
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-05-17 16:42:29 +0700

    memasukkan nilai ke method cetak

--- Konstuctor/Konstuctor.cpp
+++ Konstuctor/Konstuctor.cpp
@@ -31,3 +31,8 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 	nim = iNim;
 	nama = iNama;
 }
+
+void mahasiswa::cetak() {
+	cout << "NIM = " << nim << endl;
+	cout << "Nama = " << nama << endl;
+}

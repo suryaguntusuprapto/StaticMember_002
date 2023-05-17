da9abc877b3c08fc8db2d94edb38997a242d3657
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-05-17 16:33:51 +0700

    membuat class mahasiswa

--- Konstuctor/Konstuctor.cpp
+++ Konstuctor/Konstuctor.cpp
@@ -2,3 +2,14 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+private:
+	int nim;
+	string nama;
+public:
+	mahasiswa();
+	mahasiswa(int);
+	mahasiswa(string);
+	mahasiswa(int iNim, string iNama);
+	void cetak();
+};
